759fcd88be0981cf499437ee7357b9ad494554cf
karthikpotthuri <60749516+karthikpotthuri@users.noreply.github.com>
2020-03-05 07:26:23 +0530

    Add files via upload
    just commited

--- CMD.py
+++ CMD.py
@@ -1,26 +1,15 @@
-import sys
-a=int(sys.argv[1])
-b=int(sys.argv[2])
 def add(a,b):
     sum=a+b
-    print("the sum",sum)
+    return sum
 
 def sub(a,b):
     sub=a-b
-    print("the sum",sub)
+    return sub
 
 def mul(a,b):
     mul=a*b
-    print("the sum",mul)
+    return mul
 
 def div(a,b):
-    if b==0:
-        print("division not possible")
-    else:
-        div=a/b
-        print("the sum",div)
-
-add(a,b)
-sub(a,b)
-mul(a,b)
-div(a,b)
+    div=a/b
+    return div
